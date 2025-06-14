95d6ff8e5db36ea14b57402c45d5cba655812f2d
aTurtleGod <markus.r740@slcstudents.org>
2025-06-14 03:05:32 +0000

    ooooooooooooʻooo

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,21 @@
 const CACHE_NAME = 'dpmaker-cache-v1';
 const URLS_TO_CACHE = [
   '/dpmaker/',
-    '/dpmaker/index.html',
-      '/dpmaker/manifest.json',
-        '/dpmaker/notepad.jpg'
-            // Add other assets as needed
-            ];
+  '/dpmaker/index.html',
+  '/dpmaker/manifest.json',
+  '/dpmaker/notepad.jpg'
+  // Add more assets if needed
+];
 
-            self.addEventListener('install', event => {
-              event.waitUntil(
-                  caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
-                    );
-                      self.skipWaiting();
-                      });
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
 
-                      self.addEventListener('fetch', event => {
-                        event.respondWith(
-                            caches.match(event.request).then(response => response || fetch(event.request))
-                              );
-                              });
-                              
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
